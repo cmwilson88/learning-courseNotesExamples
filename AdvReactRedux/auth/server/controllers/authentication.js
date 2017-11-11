f0ac600c4e45e4eea221f11f1fcc165c8cea5b22
Christopher Wilson <cmw.wilson88@gmail.com>
2017-11-11 17:41:33 -0500

    Install JWT dependency.  Create JWT on sign up

--- AdvReactRedux/auth/server/controllers/authentication.js
+++ AdvReactRedux/auth/server/controllers/authentication.js
@@ -1,4 +1,13 @@
 const User = require('../models/users');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user) {
+    const timestamp = new Date().getTime();
+    // sub stands for subject aka who this token is about
+    // iat stands for issued at time
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function(req, res, next) {
     const email = req.body.email;
@@ -26,11 +35,7 @@ exports.signup = function(req, res, next) {
            if(err) return next(err);
            
            // Respond to request indicating user was created
-            res.json({ success: true });
+            res.json({ token: tokenForUser(user) });
         });
-
-    
     })
-
-
 }
