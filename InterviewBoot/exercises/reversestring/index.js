7203a67bacb2c3cad4e83ea21635d09fe808c28b
Christopher Wilson <cmw.wilson88@gmail.com>
2017-12-09 11:10:50 -0500

    InterviewBoot : Reverse a String - other solutions | debugger process

--- InterviewBoot/exercises/reversestring/index.js
+++ InterviewBoot/exercises/reversestring/index.js
@@ -7,7 +7,33 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  return str.split('').reverse().join('');
+  // debugger;
+  // console: 
+    // nodejs inspect index.js when in proper dir
+    // c, cont, continue -> repl to enter a js console
+  return str.split('').reduce((rev, char) => char + rev, '')
 }
 
+// reverse('asdf');
+
 module.exports = reverse;
+
+// function reverse(str) {
+  //   return str.split('').reverse().join('');
+  // }
+
+// function reverse(str) {
+//   let newStr = '';
+//   // Loop backwards
+//   // for (let i = str.length; i--; i>0) {
+//   //   newStr += str[i];
+//   // }
+
+//   // Loop fowards
+//   // Don't use traditional for loop syntax
+//   // Use this:
+//   for(let character of str) {
+//     newStr = character + newStr;
+//   }
+//   return newStr;
+// }
