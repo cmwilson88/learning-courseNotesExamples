c01585a342b6ad65eb3416362a6fbac51f71e39c
Christopher Wilson <cmw.wilson88@gmail.com>
2017-12-12 20:11:00 -0500

    InterviewBoot : MaxChar - Character mapping | Object.keys() | for..in loop

--- InterviewBoot/exercises/maxchar/index.js
+++ InterviewBoot/exercises/maxchar/index.js
@@ -5,6 +5,42 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const arr = str.split('');
+  let obj = {}
+  for (let character of arr) {
+    if(obj[character]) {
+      obj[character]++
+    } else {
+      obj[character] = 1
+    }
+  }
+
+  let max = 0;
+  let maxChar = '';
+  for (let char in obj) {
+    if (obj[char] > max) {
+      max = obj[char];
+      maxChar = char;
+    } 
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
+
+
+// First Attempt
+// function maxChar(str) {
+//   const arr = str.split('');
+//   let obj = {}
+//   for (character of arr) {
+//     if(obj[character]) {
+//       obj[character]++
+//     } else {
+//       obj[character] = 1
+//     }
+//   }
+//   return Object.keys(obj).reduce((a,b) => obj[a] > obj[b] ? a : b)
+// }
