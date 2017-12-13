d16e322c6aea596602f9ec721668056ef71c8f2c
Christopher Wilson <cmw.wilson88@gmail.com>
2017-12-13 12:21:49 -0500

    InterviewBoot : Capitalize Words - String.slice()

--- InterviewBoot/exercises/capitalize/index.js
+++ InterviewBoot/exercises/capitalize/index.js
@@ -7,6 +7,47 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// Second video solution
+  // Create an empty string called 'result'
+  // for each character in the string
+    // if the character to the left is a space
+      // capitalize it and add it to result
+    //else 
+      // add it to result
+// NOTE: Not always good if we are always capitalizing the first character (e.g., spanish sentences);
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+  for (let i = 1; i < str.length; i++) {
+    if (str[i-1] === ' ') {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+  return result
+}
 
 module.exports = capitalize;
+
+// First attempt
+// function capitalize(str) {
+//   return str.split(' ').map(word => {
+//     return word[0].toUpperCase() + word.slice(1);
+//   }).join(' ');
+// }
+
+
+
+// First video solution
+// function capitalize(str) {
+//   const words = [];
+
+//   for (let word of str.split(' ')) {
+//     words.push(word[0].toUpperCase() + word.slice(1));
+//   }
+
+//   return words.join(' ');
+// }
+
+
+
