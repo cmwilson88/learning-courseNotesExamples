5a01c4805b3f60ff6dea34c337f6f5e6cb42abe3
Christopher Wilson <cmw.wilson88@gmail.com>
2017-12-13 10:56:25 -0500

    InterviewBoot : Array Chunking - Array.slice() | Array.splice() | while loop

--- InterviewBoot/exercises/chunk/index.js
+++ InterviewBoot/exercises/chunk/index.js
@@ -8,6 +8,57 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// Second video solution
+  // create empty 'chunked' array
+  // create 'index' start at 0
+  // while index is less than array.length
+    // push a slice of length 'size' from 'array' into 'chunked'
+    // add 'size' to 'index'
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+  
+  while (index < array.length) {
+    chunked.push(array.slice(index, index+size))
+    index += size
+  }
+
+  return chunked;
+}
+
+
 
 module.exports = chunk;
+
+// First Attempt
+// function chunk(array, size) {
+//   let newArr = [];
+//   while (array.length) {
+//     newArr.push(array.splice(0, size))
+//   }
+//   return newArr;
+// }
+
+
+// First video solution
+  // Create empty array to hold chunks called 'chunked'
+  // For each element in the 'unchunked'array
+    // retrieve the last element in 'chunked'
+    // If last element does not exist, or if it's length is equal to chunk size
+      // Push a new chunk into 'chunked' with the current element
+    // Else add the current element into chunk
+    // =================
+    // function chunk(array, size) {
+    //   const chunked = [];
+      
+    //   for (let item of array) {
+    //     const last = chunked[chunked.length-1];
+    //     if(!last || last.length === size) {
+    //       chunked.push([item])
+    //     } else {
+    //       last.push(item)
+    //     }
+    //   }
+    
+    //   return chunked;
+    // }
--- InterviewBoot/exercises/fizzbuzz/index.js
+++ InterviewBoot/exercises/fizzbuzz/index.js
@@ -15,12 +15,12 @@
 function fizzBuzz(n) {
   for(let i = 1; i <= n; i++) {
     // is the number a multiple of 3 and 5?
-    if(n % 3 === 0 && n % 5 === 0) {
+    if(i % 3 === 0 && i % 5 === 0) {
       console.log('fizzbuzz');
-    } else if (n % 3 === 0) {
+    } else if (i % 3 === 0) {
     // is the number a multiple of 3?
       console.log('fizz')
-    } else if (n % 5 === 0) {
+    } else if (i % 5 === 0) {
     // is the number a multiple of 5?      
       console.log('buzz')
     } else {
