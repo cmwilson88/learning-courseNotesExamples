597662837730faa32b2759c70ef4781de7df8d75
Christopher Wilson <cmw.wilson88@gmail.com>
2017-12-26 11:38:17 -0500

    ReactTDD - Gift Giver BDD : Convert to BDD | describe() | beforeEach() | afterEach() | Enzyme setState()

--- ReactTDD/giftgiver/src/components/App.test.js
+++ ReactTDD/giftgiver/src/components/App.test.js
@@ -2,24 +2,40 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-const app = shallow(<App />);
 
-it('component renders correctly', () => {
-  expect(app).toMatchSnapshot();
-})
+// BDD (describe)
+describe('App', () => {
+  const app = shallow(<App />);
+  
+  it('component renders correctly', () => {
+    expect(app).toMatchSnapshot();
+  })
+  
+  it('initializes the `state` with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([]);
+  })
+  
 
-it('initializes the `state` with an empty list of gifts', () => {
-  expect(app.state().gifts).toEqual([]);
-})
+  // Describe witihn describe to group similar tests based on first step
+  describe('when clicking on `add-gift` button', () => {
+    // beforeEach works at beginning of describe and fires before each test runs
+    beforeEach( () => {
+      app.find('.btn-add').simulate('click');
+    });
 
-it('adds a new gift to `state` when clicking the `add gift` button', () => {
-  app.find('.btn-add').simulate('click');
+    // afterEach to reverse effects of beforeEach
+    afterEach( () => {
+      // Can use setState in tests via Enzyme
+      app.setState({ gifts: [] })
+    }) 
 
-  expect(app.state().gifts).toEqual([ { id: 1} ])
-})
+    it('adds a new gift to `state`', () => {
+      expect(app.state().gifts).toEqual([ { id: 1} ])
+    })
+    
+    it('adds a new gift to the rendered list', () => {
+      expect(app.find('.gift-list').children().length).toEqual(1);
+    })
+  })
 
-it('adds a new gift to the rendered list when clicking the `add gift` button', () => {
-  app.find('.btn-add').simulate('click');
-
-  expect(app.find('.gift-list').children().length).toEqual(2);
 })
