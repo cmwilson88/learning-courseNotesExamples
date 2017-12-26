e46da5966f142b96e1a939e58cc7fdb717b703ab
Christopher Wilson <cmw.wilson88@gmail.com>
2017-12-26 12:00:29 -0500

    ReactTDD - Gift Giver BDD : Gift Component BDD/TDD | simulate('change', { target { value: 'something' } }) | react-bootstrap forms

---
+++ ReactTDD/giftgiver/src/components/Gift.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import { Form, 
+         FormGroup, 
+         FormControl, 
+         ControlLabel, 
+         Button } from 'react-bootstrap';
+
+class Gift extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      person: '',
+      present: ''
+    }
+  }
+
+  render() {
+    return (
+      <Form>
+        <FormGroup>
+          <ControlLabel>Person: </ControlLabel>
+          <FormControl 
+            className="input-person" 
+            onChange={event => this.setState({ person: event.target.value })}/>
+        </FormGroup>
+      </Form>
+    )
+  }
+}
+
+export default Gift;
