34d43ecd18c7ac0e6a7ff6a1100c70df71249eb1
Christopher Wilson <cmw.wilson88@gmail.com>
2017-12-26 01:31:25 -0500

    ReactTDD - Gift Giver TDD : Snapshots | Hit 'u' key to update snapshot in test runner terminal when render has changed

--- ReactTDD/giftgiver/src/setupTests.js
+++ ReactTDD/giftgiver/src/setupTests.js
@@ -4,6 +4,5 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({
-  adapter: new Adapter(),
-  disableLifecycleMethods: true
+  adapter: new Adapter()
 })
