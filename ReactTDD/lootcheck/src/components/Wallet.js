496024824e1d7a0e718eda97a15ceb3b9b4662af
Christopher Wilson <cmw.wilson88@gmail.com>
2017-12-28 14:38:06 -0500

    ReactTDD - Lootcheck Redux : Test for input udpating balance in Wallet state

--- ReactTDD/lootcheck/src/components/Wallet.js
+++ ReactTDD/lootcheck/src/components/Wallet.js
@@ -2,10 +2,29 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 
 export class Wallet extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      balance: undefined
+    }
+  }
+
+  updateBalance = event => {
+    this.setState({
+      balance: parseInt(event.target.value, 10)
+    })
+  }
+
   render() {
     return (
       <div>
         <h3 className="balance">Wallet balance: {this.props.balance}</h3>
+        <br />
+        <input 
+          type="text" 
+          className="input-wallet"
+          onChange={this.updateBalance} />
       </div>
     )
   }
