ae3da5e20fd5937a82b34e59fccb7e4331c8cdfd
Christopher Wilson <cmw.wilson88@gmail.com>
2018-01-11 23:37:55 -0500

    InterviewBoot : Vowels - RegExp | .includes | .match()

--- InterviewBoot/exercises/vowels/index.js
+++ InterviewBoot/exercises/vowels/index.js
@@ -7,6 +7,42 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// Video Solution #2
+const vowels = str => {
+  // g makes sure we don't stop at first match
+  // i stands for case insensitive
+  
+  // matches returns an array of matches or null
+  const matches = str.match(/[aeiou]/gi)
+  return matches ? matches.length : 0
+}
 
 module.exports = vowels;
+
+
+// First attempt
+// function vowels(str) {
+//   const arr = str.toLowerCase().split('');
+//   let count = 0;
+//   for(let i = 0; i < arr.length; i++) {
+//     if(/[aeiou]$/.test(arr[i])) {
+//       count++;
+//     }
+//   }
+//   return count
+// }
+
+// Video Solution #1
+// Iterative Solution
+// const vowels = str => {
+//   let count = 0;
+//   const checker = ['a', 'e', 'i', 'o', 'u']
+
+//   for(let char of str.toLowerCase()) {
+//     if(checker.includes(char)) {
+//       count++
+//     }
+//   }
+
+//   return count;
+// }
