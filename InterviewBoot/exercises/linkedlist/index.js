40026a3f68cf9e358d28ab22b8ccb7a553cae9aa
Christopher Wilson <cmw.wilson88@gmail.com>
2018-01-21 22:17:09 -0500

    InterviewBoot : LinkedList - insertAt()

--- InterviewBoot/exercises/linkedlist/index.js
+++ InterviewBoot/exercises/linkedlist/index.js
@@ -126,6 +126,18 @@ class LinkedList {
     }
     previous.next = previous.next.next;
   }
+
+  insertAt(data, index) {
+    if (!this.head || index === 0) {
+      this.insertFirst(data);
+      return;
+    }
+
+    const previous = this.getAt(index - 1) || this.getLast();
+    const node = new Node(data, previous.next);
+    previous.next = node;
+
+  }
 }
 
 module.exports = { Node, LinkedList };
